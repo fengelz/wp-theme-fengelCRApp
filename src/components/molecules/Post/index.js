742d8630379b738324b622e32b01dcc4c26c6e1c
Sune <fengel@gmail.com>
2018-06-14 22:48:06 +0200

    Added react-helmet

--- src/components/molecules/Post/index.js
+++ src/components/molecules/Post/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { Helmet } from 'react-helmet'
 import moment from 'moment'
 
 import Html from '../../atoms/Html'
@@ -7,12 +8,16 @@ import './styles.css'
 
 class Post extends PureComponent {
   render() {
-    const {post} = this.props
+    const { post } = this.props
     return (
       <article className="mol-28055d40-87fd-4b8c-925d-c061f7d075d2">
-      <h1>{post.title.rendered}</h1>
-      <time>{moment(post.date).format('Do MMM YYYY')}</time>
-      <Html content={post.content.rendered} />
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Fengel.com | {post.title.rendered}</title>
+        </Helmet>
+        <h1>{post.title.rendered}</h1>
+        <time>{moment(post.date).format('Do MMM YYYY')}</time>
+        <Html content={post.content.rendered} />
       </article>
     )
   }
--- src/components/organisms/Master/index.js
+++ src/components/organisms/Master/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 
 import './styles.css'
 
@@ -6,6 +7,10 @@ class Master extends Component {
   render() {
     return (
       <main className="org-05396e7b-bd4b-4913-a46a-310f301f2b77">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Fengel.com | Bitch and Peaces</title>
+        </Helmet>
         {this.props.children}
       </main>
     )
