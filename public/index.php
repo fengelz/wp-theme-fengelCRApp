0854b603d0a14ccef1416ab409b18518fc1b32e6
Sune <fengel@gmail.com>
2018-07-04 13:53:47 +0200

    Added wp files

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+ ?>
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Sune Fengel | Bitch and Peaces</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel='stylesheet' id='style-css'  href='/wp-content/themes/wp-theme-fengelCRApp/static/css/main.fe0d0930.css?ver=1.1' type='text/css' media='all' />
+  </head>
+  <body>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+    <script type='text/javascript' src='/wp-content/themes/wp-theme-fengelCRApp/static/js/main.830e807b.js?ver=4.9.6'></script>
+
+  </body>
+</html>
