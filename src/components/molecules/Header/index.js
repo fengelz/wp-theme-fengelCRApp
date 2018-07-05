cc724abdbddfac27b4940eb2efe46376f5fd85d7
Sune <fengel@gmail.com>
2018-07-05 12:38:34 +0200

    removed consople.log

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -4,7 +4,6 @@ import ToggleMenuButton from '../../atoms/ToggleMenuButton'
 
 class Header extends React.Component {
   render() {
-    console.log(this.props)
     return (
       <header className={`mol-b684bb38-10a7-4f15-81fe-8e7d413b4d33 ${this.props.close ? 'close' : ''}`}>
         <ToggleMenuButton
